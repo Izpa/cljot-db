3d030e7760e53e6e7e462b963901fc79eea45161
izpa <ispius.prime@gmail.com>
2025-06-10 11:10:27 +0300

    [wip] dialogue

--- src/dialogue.clj
+++ src/dialogue.clj
@@ -6,6 +6,15 @@
    [utils :refer [pformat]]
    [clojure.string :as str]))
 
+(defmethod ig/init-key ::user-states [_ _]
+  (atom {}))
+
+(defmethod ig/init-key ::update-user-state [_ {:keys [user-states]}]
+  (fn
+    ([user-id state-name] (swap! user-states assoc user-id {:state-name state-name}))
+    ([user-id state-name data] (swap! user-states assoc user-id {:state-name state-name
+                                                                 :state-data data}))))
+
 (defn telegram-send
   ([bot to-id main-content] (telegram-send bot to-id main-content {}))
   ([bot to-id main-content additional-content]
@@ -28,14 +37,6 @@
   [text]
   (when text (str/starts-with? text "/")))
 
-(defmethod ig/init-key ::user-answer [_ _]
-  (fn [msg answer]
-    (answer msg)))
-
-(defmethod ig/init-key ::admin-answer [_ _]
-  (fn [msg answer]
-    (answer msg)))
-
 (defmethod ig/init-key ::check-invite [_ {:keys [invite]}]
   (fn [msg-text]
     (when invite
@@ -44,40 +45,39 @@
              (= invite received-invite)
              (empty? args))))))
 
-(defmethod ig/init-key ::user [_ {:keys [admin? db-execute! check-invite]}]
+(defmethod ig/init-key ::user [_ {:keys [admin? select-user insert-user! check-invite]}]
   (fn [user-id text chat answer]
-    (if-let [user (db-execute! {:select :*
-                                :from :tg-user
-                                :where [:= :id user-id]}
-                               true)]
+    (if-let [user (select-user user-id)]
       user
       (when (or (admin? user-id)
                 (check-invite text))
-        (db-execute! {:insert-into :tg-user
-                      :values [(select-keys chat
-                                            [:id
-                                             :username
-                                             :last_name
-                                             :first_name])]}
-                     true)
+        (insert-user! chat)
         (answer "Добро пожаловать!")))))
 
+(defmethod ig/init-key ::default-state [_ {:keys [admin?]}]
+  (fn [{{:keys [id]
+         :as _chat} :chat
+        :keys [_text]
+        :as msg}
+       _state-data
+       answer]
+    (let [_admin? (admin? id)]
+      (answer msg))))
+
 (defmethod ig/init-key ::process-msg [_ {:keys [telegram-send
-                                                admin?
-                                                user-answer
-                                                admin-answer
+                                                user-states
+                                                states
                                                 user]}]
   (fn [{{:keys [id]
          :as chat} :chat
         :keys [text]
         :as msg}]
     (let [answer (partial telegram-send id)
-          admin? (admin? id)
           user (user id text chat answer)]
+      (log/info user)
       (if id
         (if user
-          (if admin?
-            (admin-answer msg answer)
-            (user-answer msg answer))
+          (let [{:keys [state-name state-data]} (get @user-states id {:state-name :default})]
+                  ((get states state-name) msg state-data answer))
           (answer "This is private bot. Access denied."))
         (log/warn "strange message without chat-id: " (pformat msg))))))
